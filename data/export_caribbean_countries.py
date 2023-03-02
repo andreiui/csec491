541d8f335c52e7b7779b8e7c6e722b0ad443c656
Andrei Pascu <andrei.pascu@yale.edu>
2023-03-02 17:23:16 -0500

    Improved Caribbean COVID-19 data generation

--- data/export_caribbean_countries.py
+++ data/export_caribbean_countries.py
@@ -3,7 +3,7 @@
 """
 TBD
 --------------------------------------------------
-Created on 02/28/2023. Last updated on 02/28/2023.
+Created on 02/28/2023. Last updated on 03/02/2023.
 Written by Andrei Pascu, Yale College '23.
 --------------------------------------------------
 """
@@ -11,42 +11,100 @@ Written by Andrei Pascu, Yale College '23.
 from sys import stderr
 from csv import reader, writer
 
-""" Console helper functions """
-console_info  = lambda msg: print(f"\033[34mINFO:\033[00m  {msg}", file=stderr)
-console_warn  = lambda msg: print(f"\033[93mWARN:\033[00m  {msg}", file=stderr)
-console_fatal = lambda msg: print(f"\033[31mFATAL:\033[00m {msg}", file=stderr)
+# Console helper methods
+console_info = lambda msg: print(f"\033[34mINFO:\033[00m {msg}", file=stderr)
+console_warn = lambda msg: print(f"\033[93mWARN:\033[00m {msg}", file=stderr)
+console_done = lambda msg: print(f"\033[96mDONE:\033[00m {msg}", file=stderr)
 
-""" Translation for JHU raw data files to Carribean processed data """
-RAW_TO_PROC_FNS: dict[str, str] = {
-    "raw/time_series_covid19_confirmed_global.csv": "proc/time_series_covid19_confirmed_caribbean.csv",
-    "raw/time_series_covid19_deaths_global.csv":    "proc/time_series_covid19_deaths_caribbean.csv",
-    "raw/time_series_covid19_recovered_global.csv": "proc/time_series_covid19_recovered_caribbean.csv",
+# Translation for JHU raw data files to Carribean generated data
+FILENAMES: dict[str, tuple[str, str]] = {
+    "gen/time_series_covid19_confirmed_caribbean.csv": (
+        "raw/time_series_covid19_confirmed_global.csv",
+        "raw/time_series_covid19_confirmed_US.csv",
+    ),
+    "gen/time_series_covid19_deaths_caribbean.csv": (
+        "raw/time_series_covid19_deaths_global.csv",
+        "raw/time_series_covid19_deaths_US.csv",
+    ),
 }
 
-""" List of Caribbean countries to filter; should match names in JHU raw files """
-CARIBBEAN_COUNTRIES: set[str] = {
+# List of Caribbean countries, states and territories to filter;
+# should match names in JHU raw files
+GLOBAL_CARIBBEAN: set[str] = set([
     "Antigua and Barbuda",
-}
+    "Bahamas",
+    "British Virgin Islands", # UK
+    "Cayman Islands", # UK
+    "Guadeloupe", # FR
+    "Saint Vincent and the Grenadines",
+    "Turks and Caicos Islands", # UK
+])
+US_TERRITORIES: set[str] = set([
+    "American Samoa",
+    "Guam",
+    "Northern Mariana Islands",
+    "Puerto Rico",
+    "Virgin Islands",
+])
 
-""" Iterate through JHU raw files and rows of countries and write Caribbean data """
+# Iterate through JHU raw files and write generated Caribbean data
+# following the *_global.csv format
 if __name__ == "__main__":
-    for raw_fn, proc_fn in RAW_TO_PROC_FNS.items():
-        # Maintain list of countries
-        global_countries: set[str] = set()
+    for gen_fn, (raw_global_fn, raw_us_fn) in FILENAMES.items():
+        # Maintain a list of regions
+        all_regions: set[str] = set()
 
         # Open input and output .csv files
-        with open(raw_fn, "r") as file_in, open(proc_fn, "w+") as file_out:
+        # First, open global files and filter for Carribean countries and states
+        with open(gen_fn, "w+") as file_out, open(raw_global_fn, "r") as file_in:
             out = writer(file_out)
             for i, row in enumerate(reader(file_in, delimiter=",")):
-                if i == 0 or row[1] in CARIBBEAN_COUNTRIES:
+                if i == 0 or not GLOBAL_CARIBBEAN.isdisjoint(row[0:2]):
                     out.writerow(row)
                     if i != 0:
-                        global_countries.add(row[1])
+                        # Add Carribean country or state to list of regions
+                        all_regions.add(row[int(row[1] in GLOBAL_CARIBBEAN)])
+            
+            # Output completion of global .csv processing
+            console_info(f"\"{raw_global_fn}\": completed processing.")
 
-            # Output warning if any specified countries are not in JHU time series
-            for caribbean_country in CARIBBEAN_COUNTRIES.difference(global_countries):
-                console_warn(f"\"{raw_fn}\": \"{caribbean_country}\" country name not found.")
-        
-        # Output completion of global to Caribbean filtering
-        console_info(f"\"{raw_fn}\": completed export to \"{proc_fn}\"")
+        # Output warning any countries that are not in the JHU time series
+        for country in GLOBAL_CARIBBEAN.difference(all_regions):
+            console_warn(f"\"{raw_global_fn}\": country name \"{country}\" not found.")
+
+        # Second, open U.S. files and filter for U.S. territories, including
+        # American Samoa, Guam and the Northern Mariana Islands
+        all_regions: set[str] = set()
+        # For Puerto Rico, multiple rows on a per-province basis are combined
+        pr_raw_data: list[list[str]] = []
+
+        with open(gen_fn, "a") as file_out, open(raw_us_fn, "r") as file_in:
+            out = writer(file_out)
+            for i, row in enumerate(reader(file_in, delimiter=",")):
+                if i == 0 or row[6] not in US_TERRITORIES:
+                    continue
+                if row[6] == "Puerto Rico":
+                    pr_raw_data.append(row[11:])
+                else:
+                    out.writerow(row[6:10] + row[11:])
+                
+                # Add U.S. territory to list of regions
+                all_regions.add(row[6])
+
+            # Calculate and write Puerto Rico data
+            if "Puerto Rico" in US_TERRITORIES:
+                out.writerow(
+                    ["Puerto Rico", "US" , "18.2208", "-66.5901"] +
+                    [sum(int(d) for d in data) for data in zip(*pr_raw_data)]
+                )
+            
+            # Output completion of U.S. .csv processing
+            console_info(f"\"{raw_us_fn}\": completed processing.")
+
+        # Output warning any countries that are not in the JHU time series
+        for country in US_TERRITORIES.difference(all_regions):
+            console_warn(f"\"{raw_us_fn}\": country name \"{country}\" not found.")
+
+        # Output completion of Caribbean .csv generation
+        console_done(f"\"{gen_fn}\": completed exporting.")
         
