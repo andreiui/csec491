70d01dfe8e1cfa196a11c3e9de2830eaf4621996
Andrei Pascu <andrei.pascu@yale.edu>
2023-04-03 12:27:49 -0400

    Implemented data syncing to code

--- sync_data.sh
+++ sync_data.sh
@@ -4,13 +4,19 @@
 # Script for syncing the "data/countries" folder
 # into the "code" directory.
 # --------------------------------------------------
-# Created on 03/30/2023. Last updated on 03/30/2023.
+# Created on 03/30/2023. Last updated on 04/03/2023.
 # Written by Andrei Pascu, Yale College '23.
 # --------------------------------------------------
 #
 
-# Recursively remove all contents in "code/countries"
-rm -r ./code/countries
-
-# Recursively copy the "countries" subfolder from "data" into "code"
-cp -r ./data/countries ./code
+# Iterate through each Excel file
+for pathname in ./data/countries/*.xlsx; do
+    # Remove .xslx and path
+    country=${pathname%.xlsx}
+    country=${country##*/}
+    # Create directory if it does not exist
+    mkdir -p "./code/$country"
+    # Convert Excel file to .csv
+    ssconvert --export-file-per-sheet --export-options "sheet=Data" $pathname "./code/$country/%s.csv"
+    echo "Converted to .csv file: $country.xlsx"
+done
